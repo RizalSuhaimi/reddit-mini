3332be6027a7f8a651bb70628202f93f28802272
Rizal Suhaimi <105201048+incursio16@users.noreply.github.com>
2024-12-14 17:16:33 +0800

    continue with basic app

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { 
+    loadRedditPosts,
+    selectRedditPosts,
+    selectErrorMessage,
+    isLoading
+} from "../Posts/RedditPostsSlice";
 
 const HomePage = () => {
+    const dispatch = useDispatch();
+    const redditPosts = useSelector(selectRedditPosts)
+    const isLoadingRedditPosts = useSelector(isLoading);
+    const redditPostsErrorMessage = useSelector(selectErrorMessage);
+
+    useEffect(() => {
+        dispatch(loadRedditPosts());
+    }, [dispatch]);
+
+    if (isLoadingRedditPosts) {
+        return <div>Content is loading</div>
+    } else if (redditPostsErrorMessage) {
+        return <div>{redditPostsErrorMessage}</div>
+    }
+
     return (
         <div>
-            <h2>What Do Snails Eat?</h2>
+            <h2>Trending in Reddit</h2>
+            <ul>
+                {redditPosts ? 
+                    redditPosts.data.children.map((post) => (
+                    <li>{post.data.title}</li>
+                    ))
+                    :
+                    <p>Content is loading</p>
+                }
+            </ul>
         </div>
     )
 }
